a2cf64c19deb10b25c3dda74376ad9844f3b737a
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2020-12-10 20:07:46 +0300

    Update input_parser.py
    - Choice for numba is implemented. Hence, there is a need for one more placeholder when calling function.
    - Order of  blocks in the input file is not important, but the order in the return function must be followed when calling parser.
    - Error catching is missing; it'll be added soon.

--- input_parser/input_parser.py
+++ input_parser/input_parser.py
@@ -10,23 +10,28 @@ then, reads file and parses it in a manner that:
 --------------------------------------------------
 DICTIONARY
 *******
-defines two dictionaries, sub_params and agent_params,
+defines two dictionaries, sub_params and slider_params,
 by indexing from related block names till end sign '/'
 --------------------------------------------------
-(!) block order is of importance (see Ln 31, Col 80)
+(!) block order is 'not' of importance in the input file
+(!) however, it is vital to call parser in the order:
+            substrate - slider - numba 
+(see paragraph below for detailed explanation)
 --------------------------------------------------
 CALLING
 *******
 when creating a class, parser should be called with a placeholder
 so that only related parameters are assigned. examples as follows:
-subs_params, _ = parse('input.txt')
-_ , agent_params = parse('input.txt')
+        subs_param, _ , _ = parse('input.txt')
+        _ , slid_param, _ = parse('input.txt')
 """
 
 def parse(file):
     with open(file, 'r') as f:
         params = [param.split(' = ') for param in f.read().lower().splitlines()]
-        
-    sub_param = dict(params[params.index(['&substrate'])+1:params.index(['/'])])
-    agent_param = dict(params[params.index(['&slider'])+1:params.index(['/'], 9)])
-    return sub_param, agent_param
+        # add error catcher
+    subs_ind, slid_ind, numba_ind = params.index(['&substrate']), params.index(['&slider']), params.index(['&numba'])
+    subs_param = dict(params[subs_ind+1:params.index(['/'], subs_ind)])
+    slid_param = dict(params[slid_ind+1:params.index(['/'], slid_ind)])
+    use_numba = dict(params[numba_ind+1:params.index(['/'], numba_ind)])
+    return subs_param, slid_param, use_numba
