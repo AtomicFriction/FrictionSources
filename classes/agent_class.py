23b80ea4f0a6e8ed27dacbe66b094a2689275c28
Bartu Yaman <e235581@metu.edu.tr>
2021-01-05 00:16:46 +0300

    Update agent_class.py

--- classes/agent_class.py
+++ classes/agent_class.py
@@ -1,97 +1,41 @@
 import numpy as np
 from input_parser import parse
 
-""""
+"""
 Uses the input data given by the user to initialize a sliding agent.
-""""
+"""
 
+_ , agent_param, _ = parse('input.txt') ## Use the parser either here of inside the Run class.
 
-## Agent is the parent class.
+## Agent class.
 class Agent:
 
     def __init__(self):
         ## Mass of the agent.
-        self.m = agent_params['mass']
+        self.m = float(agent_param['mass'])
         ## Spring constant between slider and agent.
-        self.k = agent_params['k']
-        self.hooking_point = hooking_point_slider_agent
+        self.k = float(agent_param['k'])
         ## The constant sigma for The Lennard Jones interaction.
-        self.sigma = agent_params['sigma']
+        self.sigma = float(agent_param['sigma'])
         ## The constant epsilon for The Lennard Jones interaction.
-        self.epsilon = agent_params['epsilon']
+        self.epsilon = float(agent_param['epsilon'])
         ## Shape of the agent.
-        self.shape = agent_params['shape']
+        self.shape = agent_param['shape']
+        ## Initialize agent position, velocity and acceleration as (1 x 3) arrays depending on the user selected shape.
+        if (self.shape == "single"):
+            self.pos = [[float(agent_param["agent_pos"].split(" ")[0]), float(agent_param["agent_pos"].split(" ")[1]), float(agent_param["agent_pos"].split(" ")[2])]]
+            self.vel = np.zeros(1, 3)
+            self.acc = np.zeros(1, 3)
 
-
-    def LJ_Interaction(disp, sigma, epsilon):
-
-        ## Matrix that contains the forces due to the Lennard Jones interaction.
-        lj_force = np.zeros(np.shape(R))
-        ## "N" is needed for the iteration later in the loop.
-        N = disp.shape[1]
-        ## "Rx" holds the x-axis displacements here.
-        Rx = disp[0, :]
-        ## "Ry" holds the y-axis displacements here.
-        Ry = disp[1, :]
-        ## "Ry" holds the z-axis displacements here.
-        Rz = disp[2, :]
-
-        ## Loop that computes the Lennard Jones interaction forces.
-        for n in range(1, N - 1):
-
-            norm1 = np.linalg.norm(disp[:, n + 1] - disp[:, n])
-            norm2 = np.linalg.norm(disp[:, n - 1] - disp[:, n])
-
-            lj_force[0, n] = (((48 / (Rx[n + 1] - Rx[n])) * ((1 / ((Rx[n + 1] - Rx[n]) ** 6)) ** 2) - ((1 / ((Rx[n + 1] - Rx[n]) ** 6)) / 2)) * (Rx[n + 1] - Rx[n]) / norm1) + (((48 / (Rx[n - 1] - Rx[n])) * ((1 / ((Rx[n - 1] - Rx[n]) ** 6)) ** 2) - ((1 / ((Rx[n - 1] - Rx[n]) ** 6)) / 2)) * (Rx[n - 1] - Rx[n]) / norm2)
-            lj_force[1, n] = (((48 / (Ry[n + 1] - Ry[n])) * ((1 / ((Ry[n + 1] - Ry[n]) ** 6)) ** 2) - ((1 / ((Ry[n + 1] - Ry[n]) ** 6)) / 2)) * (Ry[n + 1] - Ry[n]) / norm1) + (((48 / (Ry[n - 1] - Ry[n])) * ((1 / ((Ry[n - 1] - Ry[n]) ** 6)) ** 2) - ((1 / ((Ry[n - 1] - Ry[n]) ** 6)) / 2)) * (Ry[n - 1] - Ry[n]) / norm2)
-            lj_force[2, n] = (((48 / (Rz[n + 1] - Rz[n])) * ((1 / ((Rz[n + 1] - Rz[n]) ** 6)) ** 2) - ((1 / ((Rz[n + 1] - Rz[n]) ** 6)) / 2)) * (Rz[n + 1] - Rz[n]) / norm1) + (((48 / (Rz[n - 1] - Rz[n])) * ((1 / ((Rz[n - 1] - Rz[n]) ** 6)) ** 2) - ((1 / ((Rz[n - 1] - Rz[n]) ** 6)) / 2)) * (Rz[n - 1] - Rz[n]) / norm2)
-
-
-        return lj_force
-
-
-    def SingleAtom():
-        ## Position of the single atom from the user.
-        if(dim == 1):
-            pos_single = pos_single = [float(agent_params["agent_pos"].split(" ")[0])]
-        elif(dim == 2):
-            pos_single = [float(agent_params["agent_pos"].split(" ")[0]), float(agent_params["agent_pos"].split(" ")[1])]
-        elif(dim == 3):
-            pos_single = [float(agent_params["agent_pos"].split(" ")[0]), float(agent_params["agent_pos"].split(" ")[1]), float(agent_params["agent_pos"].split(" ")[2])]
-
-        else:
+        elif (self.shape == "hemisphere"):
+            ## Not in use right now.
             pass
 
-        print("Given dimension value is %s. Therefore, the agent has %s positional arguements." % (dim, str(len(pos_single))))
 
-        return pos_single
-
-
-    def Hemisphere():
-        ## Hemisphere shape will not be used for now.
-        pass
-
-
-    def Shape_Select(shape):
-        if (shape == "single"):
-            SingleAtom()
-        ## Hemisphere shape will not be used for now.
-        elif (shape == "hemisphere"):
-            ##Hemisphere_Shape()
-            print("Hemisphere shape is not available right now. Default shape 'single' will be used.")
-            SingleAtom()
-        else:
-            ## Raise an error and use a predefined default shape for the agent.
-            print("An invalid shape was specified in the input. Default shape 'single' will be used.")
-            SingleAtom()
-        return agent_disp
-
-
-## Slider is initialized as a child class of the parent class Agent.
-class Slider(Agent):
+## Slider will be defined as a class on its own.
+class Slider():
 
     def __init__(self):
 
-        self.velocity = slider_velocity
-
-        pass
+        self.pos = [[]]
+        self.vel = [[]]
