56c70f1cf3509396bcc3f6ab3dc9711c84088dcd
Bartu Yaman <e235581@metu.edu.tr>
2021-01-06 19:34:17 +0300

    Update agent_class.py

--- classes/agent_class.py
+++ classes/agent_class.py
@@ -21,21 +21,17 @@ class Agent:
         self.epsilon = float(agent_param['epsilon'])
         ## Shape of the agent.
         self.shape = agent_param['shape']
+
+        self.slider_pos = np.zeros((1, 3))
+        self.slider_vel = np.array([[1, 0, 0]])
         ## Initialize agent position, velocity and acceleration as (1 x 3) arrays depending on the user selected shape.
         if (self.shape == "single"):
             self.pos = [[float(agent_param["agent_pos"].split(" ")[0]), float(agent_param["agent_pos"].split(" ")[1]), float(agent_param["agent_pos"].split(" ")[2])]]
-            self.vel = np.zeros(1, 3)
-            self.acc = np.zeros(1, 3)
+            self.vel = np.zeros((1, 3))
+            self.acc = np.zeros((1, 3))
+
 
         elif (self.shape == "hemisphere"):
             ## Not in use right now.
             pass
 
-
-## Slider will be defined as a class on its own.
-class Slider():
-
-    def __init__(self):
-
-        self.pos = [[]]
-        self.vel = [[]]
