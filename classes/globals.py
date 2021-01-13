d707bf92fe5933800d4a73e290b13846a3ceb594
Bartu Yaman <e235581@metu.edu.tr>
2021-01-14 00:15:24 +0300

    Update globals.py

--- classes/globals.py
+++ classes/globals.py
@@ -3,7 +3,9 @@ from input_parser import parse
 
 
 _, run_param, _, _ = parse('input.txt')
+gen_param, _, _, _ = parse('input.txt')
 
 
 dt = float(run_param['dt'])
 integrator = (run_param['integ'])
+cutoff = float(gen_param['cutoff'])
