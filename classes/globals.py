1b14c2100943f79a53d107a175965df77cb5dee7
Bartu Yaman <e235581@metu.edu.tr>
2021-01-25 16:37:31 +0300

    Update globals.py

--- classes/globals.py
+++ classes/globals.py
@@ -2,10 +2,24 @@
 from input_parser import parse
 
 
-_, run_param, _, _ = parse('input.txt')
 gen_param, _, _, _ = parse('input.txt')
 
+cutoff = float(gen_param['cutoff'])
+##################################################
+##################################################
+_, run_param, _, _ = parse('input.txt')
 
 dt = float(run_param['dt'])
 integrator = (run_param['integ'])
-cutoff = float(gen_param['cutoff'])
+##################################################
+##################################################
+_, _, subs_param, _ = parse('input.txt')
+
+num = int(subs_param['num'])
+##################################################
+##################################################
+_, _, _, agent_param = parse('input.txt')
+
+constrain = str(agent_param['constrain'])
+##################################################
+##################################################
