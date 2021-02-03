a43dc13de73ec6c56eba1cdde7c0fec10341e888
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2021-02-03 15:15:54 +0300

    Parsing for thermostat and analysis blocks
    Parsing process for thermostat and analysis blocks is implemented. Since the function returns two new features, it should be called with two more placeholder bottoms, namely '_' .

--- input_parser/input_parser.py
+++ input_parser/input_parser.py
@@ -2,15 +2,18 @@ def parse(file):
     with open(file, 'r') as f:
         params = [param.split(' = ') for param in f.read().lower().splitlines()]
 
-    gen_ind, agent_ind = params.index(['&general']), params.index(['&slider'])
-    run_ind, subs_ind = params.index(['&run']),  params.index(['&substrate'])
+    gen_ind, prot_ind = params.index(['&general']), params.index(['&protocol'])
+    anal_ind, subs_ind = params.index(['&analysis']), params.index(['&substrate'])
+    slid_ind, thermo_ind = params.index(['&slider']), params.index(['&thermostat'])
 
     gen = dict(params[gen_ind+1:params.index(['/'], gen_ind)])
-    run = dict(params[run_ind+1:params.index(['/'], run_ind)])
+    prot = dict(params[prot_ind+1:params.index(['/'], prot_ind)])
+    anal = dict(params[anal_ind+1:params.index(['/'], anal_ind)])
     subs = dict(params[subs_ind+1:params.index(['/'], subs_ind)])
-    agent = dict(params[agent_ind+1:params.index(['/'], agent_ind)])
+    slid = dict(params[slid_ind+1:params.index(['/'], slid_ind)])
+    thermo = dict(params[thermo_ind+1:params.index(['/'], thermo_ind)])
 
-    # raise error in conditions below
+    # raise error in case of the conditions below
     try:
         dim = int(subs['dim'])
         layers = int(subs['layers'])
@@ -18,8 +21,11 @@ def parse(file):
         print("\n***\nEither number of dimensions or layers is not an integer.", \
         "\nPlease enter integer numbers for dimension and layer numbers.\n***\n")
     else:
-        if (dim == 1 or dim == 2) and (layers != 1):
+        if not (dim >= 1 and dim <= 3):
+            print("\n***\nUnexpected dimension.", \
+            "\nPlease confirm that the dimension is an integer from one to three.\n***\n")
+        elif (dim == 1 or dim == 2) and (layers != 1):
             print("\n***\nDimension and layer numbers are not matched.\nRecall that number", \
             "of layers must be equal to one where the system is one or two-dimensional.\n***\n")
 
-    return gen, run, subs, agent
+    return gen, prot, anal, subs, slid, thermo
