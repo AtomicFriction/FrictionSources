07121aea76750070562b0c39e439c19c1b03edf9
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2021-02-08 16:52:19 +0300

    Langevin is updated, but is not ready
    Definition of the variable deriv_wiener, which is white noise being the derivative of Wiener process, is still lacked.

--- classes/thermostats.py
+++ classes/thermostats.py
@@ -1,6 +1,9 @@
 import numpy as np
 import globals
 from tools import SafeDivision
+from input_parser import parse
+
+
 
 """
 Since the constants below will be used in the functions,
@@ -9,7 +12,7 @@ are defined here, so whenever one of the functions below
 is called, the constants must be also called.
 """
 
-def vel_rescale(T, mass, V):
+def vel_rescale(T_aim, mass, V):
     """
     -> Velocity Rescaling Thermostat.
     -> Calculates the instantaneous temperature of the system using the equipartition theorem, T_inst.
@@ -18,10 +21,10 @@ def vel_rescale(T, mass, V):
     """
     num_bound = 4 * (globals.num - 1)
     T_inst =  mass * np.sum(V ** 2) / (3 * globals.boltz * num_bound)
-    L = (T / T_inst) ** (1 / 2)
+    L = (T_aim / T_inst) ** (1 / 2)
     return L
 
-def berendsen(T, mass, V, coup_param):
+def berendsen(T_aim, mass, V, coup_param):
     """
     -> Berendsen Thermostat.
     -> Calculates the instantaneous temperature of the system using the equipartition theorem, T_inst.
@@ -31,19 +34,18 @@ def berendsen(T, mass, V, coup_param):
     """
     num_bound = 4 * (globals.num - 1)
     T_inst =  mass * np.sum(V ** 2) / (3 * globals.boltz * num_bound)
-    L = (1 + (globals.dt / coup_param) * (SafeDivision(T,T_inst) - 1)) * (1/2)
+    L = (1 + (globals.dt / coup_param) * (SafeDivision(T_aim,T_inst) - 1)) * (1/2)
     return L
 
 def nosehoover(mass, R):
-    s = None
-    Q = None
     print("\n\nThis thermostat is not implementable yet.\n \
             If you want to proceed with another thermostat, \
             restart the program with another thermostat.\n\n")
     return quit()
 
-def langevin(mass, V, F, f):
-    gamma = None
+def langevin(T_aim, M, V, F, f):
+    deriv_wiener = None
+    F_thermo = F - globals.gamma * M * V + (2 * globals.boltz * T_aim * globals.gamma * M)**(1/2) * dW
     print("\n\nThis thermostat is not implementable yet.\n \
             If you want to proceed with another thermostat, \
             restart the program with another thermostat.\n\n")
