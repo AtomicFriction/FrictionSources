a2d4d99c72ac4344184e35c8c3030287ddad9c41
Bartu Yaman <e235581@metu.edu.tr>
2021-02-12 14:51:57 +0300

    Update interactions.py

--- classes/interactions.py
+++ classes/interactions.py
@@ -1,69 +1,85 @@
 import numpy as np
 from agent import Agent
+from substrate import Substrate
 import globals
 from tools import SafeDivision
 
+
+dev_analyze = input("Want to see forces? y/n:    ")
 
 """
 -> Calculates the total force present on the Agent.
 -> Includes the lennard-jones force and the spring force.
 """
-def AgentForce(ag_pos, subs_pos, slider_pos):
-    ## Empty arrays for the forces to fill in later on.
+def AgentForce():
     lj_force = np.zeros((1, 3))
-    spr_force = np.zeros((1, 3))
 
-    disp = ag_pos - slider_pos
-    r = np.subtract(ag_pos, subs_pos)
+    r = np.subtract(Agent.pos, Substrate.R)
     r = r.tolist()
 
-    rr = [[], [], []]
-    for i in range(3):
-        for j in range(globals.num):
-            if (r[j][i] <= globals.cutoff and r[j][i] >= -globals.cutoff and r[j][i] != 0):
-                rr[i].append(r[j][i])
+    ##print(np.shape(r))
+
+    rr = [[0, 0, 0]]
+
+    for j in range(globals.num):
+        dist_r = np.sqrt((r[j][0] ** 2) + (r[j][1] ** 2) + (r[j][2] ** 2))
+        if ((dist_r) <= globals.cutoff):
+            rr = np.concatenate((rr, [r[j]]))
+
+    rr = np.delete(rr, 0, 0)
+
+    ##print(rr)
 
     ## Lennard-Jones Force implementation in 3D.
-    lj_force[0][0] = np.sum(48 * Agent.epsilon * np.power(Agent.sigma, 12) / np.power(rr[0][0], 13) - 24 * Agent.epsilon * np.power(Agent.sigma, 6) / np.power(rr[0][0], 7))
-    lj_force[0][1] = np.sum(48 * Agent.epsilon * np.power(Agent.sigma, 12) / np.power(rr[0][1], 13) - 24 * Agent.epsilon * np.power(Agent.sigma, 6) / np.power(rr[0][1], 7))
-    lj_force[0][2] = np.sum(48 * Agent.epsilon * np.power(Agent.sigma, 12) / np.power(rr[0][2], 13) - 24 * Agent.epsilon * np.power(Agent.sigma, 6) / np.power(rr[0][2], 7))
+    lj_force[0][0] = np.sum(48 * Agent.epsilon * np.power(Agent.sigma, 12) / np.power(rr[:, 0], 13) - 24 * Agent.epsilon * np.power(Agent.sigma, 6) / np.power(rr[:, 0], 7))
+    lj_force[0][1] = np.sum(48 * Agent.epsilon * np.power(Agent.sigma, 12) / np.power(rr[:, 1], 13) - 24 * Agent.epsilon * np.power(Agent.sigma, 6) / np.power(rr[:, 1], 7))
+    lj_force[0][2] = np.sum(48 * Agent.epsilon * SafeDivision(np.power(Agent.sigma, 12), np.power(rr[:, 2], 13)) - 24 * Agent.epsilon * SafeDivision(np.power(Agent.sigma, 6), np.power(rr[:, 2], 7)))
 
     ## Hooke's Law implementation. The equiliblium length taken as an input on the x-axis.
-    spr_force[0][0] = - globals.agent_k * (disp[0][0] - globals.eq_len)
-    spr_force[0][1] = - globals.agent_k * disp[0][1]
-    spr_force[0][2] = - globals.agent_k * disp[0][2]
+    spr_force = np.zeros((1, 3))
+    diff = np.subtract(Agent.pos, Agent.slider_pos)
+    disp = np.linalg.norm(diff) - globals.eq_len
+    normalized = diff / np.linalg.norm(diff)
 
-    """
-    ## For debugging purposes.
-    print("LJ:    " + str(lj_force))
-    print("SPR:    " + str(spr_force))
-    print(np.shape(lj_force))
-    """
-
-    return (lj_force + spr_force, lj_force)
+    spr_force = -1 * globals.agent_k * disp * normalized
 
 
-def SubstrateForce(ag_pos, subs_pos, slider_pos, neigh):
-    subs_force = np.zeros(np.shape(subs_pos))
+    if (dev_analyze == "y"):
+        ## For debugging purposes.
+        print("LJ:    " + str(lj_force))
+        print("SPR:    " + str(spr_force))
+        print(np.shape(spr_force))
+    elif (dev_analyze == "n"):
+        pass
+
+    agent_force = lj_force + spr_force
+    ##print(agent_force)
+
+    return (agent_force, lj_force)
+
+
+def SubstrateForce():
+    subs_force = np.zeros(np.shape(Substrate.R))
     # index R, such that R[1:-1], to ignore boundaries
     # (at least for now, but it may be generalized to non-boundary conditions)
-    norm1 = np.linalg.norm(subs_pos[neigh[1:-1]][:, 1] - subs_pos[1:-1])
-    norm2 = np.linalg.norm(subs_pos[neigh[1:-1]][:, 0] - subs_pos[1:-1])
+    norm1 = np.linalg.norm(Substrate.R[Substrate.table[1:-1]][:, 1] - Substrate.R[1:-1])
+    norm2 = np.linalg.norm(Substrate.R[Substrate.table[1:-1]][:, 0] - Substrate.R[1:-1])
 
-    a = (globals.subs_k * (norm1 - globals.latt_const) * (subs_pos[neigh[1:-1]][:, 1] - subs_pos[1:-1]))
-    b = (globals.subs_k * (norm2 - globals.latt_const) * (subs_pos[neigh[1:-1]][:, 0] - subs_pos[1:-1]))
+    a = (globals.subs_k * (norm1 - globals.latt_const) * (Substrate.R[Substrate.table[1:-1]][:, 1] - Substrate.R[1:-1]))
+    b = (globals.subs_k * (norm2 - globals.latt_const) * (Substrate.R[Substrate.table[1:-1]][:, 0] - Substrate.R[1:-1]))
 
     subs_force[1:-1] = SafeDivision(a, norm1) + SafeDivision(b, norm2)
 
-    lj_force = AgentForce(ag_pos, subs_pos, slider_pos)[1]
+    lj_force = AgentForce()[1]
+    ##lj_force = 0
     subs_force = np.subtract(subs_force, lj_force)
 
     return subs_force
 
 
 ## A method to unify all of the force calculator functions in one. This is needed for later use in the integrators.
-def GetForces(force_select, ag_pos, subs_pos, slider_pos, neigh):
+def GetForces(force_select):
     if (force_select == "AGENT"):
-        return AgentForce(ag_pos, subs_pos, slider_pos)[0]
+        return AgentForce()[0]
     elif (force_select == "SUBSTRATE"):
-        return SubstrateForce(ag_pos, subs_pos, slider_pos, neigh)
+        return SubstrateForce()
