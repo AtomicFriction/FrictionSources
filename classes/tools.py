28d864651bc6ab161be9ddc8304fbae9c06978ee
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2021-02-23 13:31:18 +0300

    RunConf is deleted

--- classes/tools.py
+++ classes/tools.py
@@ -3,11 +3,9 @@ import globals
 
 """
 -> Constrains the motion to the desired axis by simple matrix multiplications.
-
 Input x: Constrains the motion on the x-axis. Nullifies the components of other axes.
 Input y: Constrains the motion on the y-axis. Nullifies the components of other axes.
 Else: Does nothing.
-
 """
 def constrain(direction, vel, acc):
     if (direction == "x"):
@@ -25,31 +23,8 @@ def constrain(direction, vel, acc):
 
 
 """
--> Configures the "run" parameter as a (n x 3) array. The rows contain group of instructions.
-
-For input n x y z k l m :
-    n = the number of groups of three
-    configured_array (output) = [[x, y, z],
-                                [k, l, m]]
-    x, k = Starting temperatures.
-    y, l = Target temperatures.
-    z, m = Number of steps to reach the target temperature.
-
-"""
-def RunConf():
-    conf_arr = []
-    for i in range(int(globals.run[0])):
-        conf_arr.append([float(globals.run.split(" ")[(3 * i) + 1]), float(globals.run.split(" ")[(3 * i) + 2]), float(globals.run.split(" ")[(3 * i) + 3])])
-
-    conf_arr = np.array(conf_arr)
-
-    return conf_arr
-
-
-"""
 -> Returns zero if there is a "RuntimeWarning: divide by zero encountered in true_divide" issue, which returns NaN.
 -> Int and float inputs are fine, like the ordinary "/" operator.
-
 """
 def SafeDivision(x, y):
     res = ( x / y ) if y.any() != 0 else 0
@@ -61,7 +36,6 @@ def SafeDivision(x, y):
 For inputs: f = the function to be differentiated.
             a = the point we want to find the differentiation for.
             h = step size (must be virtually zero because of the definition of this method).
-
 Output: The value of f'(a).
 """
 def NumericalDiff(f, a, h):
@@ -74,18 +48,17 @@ For input: ff etot ke pe
 Output: ['ff', 'etot', 'ke', 'pe']
 """
 def AnalysisList():
-    selection_list = (globals.data.split(" "))
-    print(selection_list)
-    if ("ke" in selection_list):
+    print(globals.data)
+    if ("ke" in globals.data):
         globals.kinetic_switch = 1
         print("kin :" + str(globals.kinetic_switch))
-    if ("pe" in selection_list):
+    if ("pe" in globals.data):
         globals.potential_switch = 1
         print("pe: " + str(globals.potential_switch))
-    if ("ff" in selection_list):
+    if ("ff" in globals.data):
         globals.ff_switch = 1
         print("ff: " + str(globals.ff_switch))
-    if ("temp" in selection_list):
+    if ("temp" in globals.data):
         globals.temp_switch = 1
         print("temp: " + str(globals.temp_switch))
 
@@ -96,3 +69,13 @@ def LogFile():
     log.write()
     log.write()
     log.write()
+        
+def KE(m, V):
+    """
+    UNITS
+    -----
+    m: mass (Dalton)-> 1.660538921 x 10e−27 kg
+    V: velocity (Angström/picoseconds)-> 10e2 m/s
+    """
+    kinergy = 1/2*m*np.sum(V**2)*(1.660538921e-23)
+    return kinergy
