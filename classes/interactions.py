1ea56bf0a84b01df1e8ad37884bb8872f89fc739
Bartu Yaman <e235581@metu.edu.tr>
2021-02-26 23:43:02 +0300

    Potential and friction calculations removed from "AgentForce()".

--- classes/interactions.py
+++ classes/interactions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.spatial import distance
+import timeit
 
 from agent import Agent
 from substrate import Substrate
@@ -9,16 +10,18 @@ from tools import SafeDivision, NumericalDiff
 
 dev_analyze = input("Want to see forces? y/n:    ")
 
+
 """
 -> Calculates the total force present on the Agent.
--> Includes the lennard-jones force and the spring force.
+-> Includes:
+            3D Lennard-Jones force calculation between agent and substrate,
+            3D spring force calculation between agent and slider.
 """
 def AgentForce():
-    ##print("AgentForce called.")
     ## Lennard-Jones Potential implementation in 3D.
     lj = []
     lj_pot = []
-
+    # Evaluates all the distance values between the agent and the substrate atoms.
     dist = distance.cdist(Agent.pos, Substrate.R, 'euclidean')
 
     cutoff = (dist != 0) & (dist < globals.cutoff) * 1
@@ -26,25 +29,19 @@ def AgentForce():
     idx = np.unique(extract[0], return_index=True)
     table = np.array_split(extract[1], idx[1])[1:]
 
-    ##print(len(table))
     if (len(table) == 1):
         for i in range(len(table[0])):
             rr = dist[0][table[0][i]]
-            dir = np.subtract(Agent.pos, Substrate.R[table[0][i]]) / rr
-            lj_force_whole = (((48 * Agent.epsilon) * ((Agent.sigma ** 12) / (rr ** 13))) - ((24 * Agent.epsilon) * ((Agent.sigma ** 6) / (rr ** 7)))) * dir
+            # We only calculate these once for efficiency.
+            globals.rr = rr
+            rr_12 = (rr ** 12)
+            rr_6 = (rr ** 6)
+            sig_12 = (Agent.sigma ** 12)
+            sig_6 = (Agent.sigma ** 6)
+            # Evaluate the direction of the force.
+            dir = (Agent.pos - Substrate.R[table[0][i]]) / rr
+            lj_force_whole = (((48 * Agent.epsilon) * ((sig_12) / (rr_12 * rr))) - ((24 * Agent.epsilon) * ((sig_6) / (rr_6 * rr)))) * dir
             lj.append(lj_force_whole)
-            """
-            print("-------------------------------------------------------")
-            print("This is force calculated directly:    " + str(((48 * Agent.epsilon) * ((Agent.sigma ** 12) / (rr ** 13))) - ((24 * Agent.epsilon) * ((Agent.sigma ** 6) / (rr ** 7)))))
-            """
-            if (globals.potential_switch == 1):
-                lj_pot = np.sum((4 * Agent.epsilon) * (((Agent.sigma / rr) ** 12) - ((Agent.sigma / rr) ** 6)))
-                """
-                h = 0.000005
-                print("This is force from potential, h = " + str(h) + ":    " + str(-(((4 * Agent.epsilon) * (((Agent.sigma / (rr + h)) ** 12) - ((Agent.sigma / (rr + h)) ** 6))) - ((4 * Agent.epsilon) * (((Agent.sigma / (rr - h)) ** 12) - ((Agent.sigma / (rr - h)) ** 6)))) / (2 * h)))
-                """
-            else:
-                pass
 
         globals.lj_force = np.sum(lj, axis = 0)
     else:
@@ -52,47 +49,23 @@ def AgentForce():
 
 
     ## Hooke's Law implementation. The equiliblium length taken as an input on the x-axis.
-    spr_force = np.zeros((1, 3))
     spr_pot = []
 
-    diff = np.subtract(Agent.pos, Agent.slider_pos)
+    diff = Agent.pos - Agent.slider_pos
     disp = np.linalg.norm(diff) - globals.eq_len
     normalized = diff / np.linalg.norm(diff)
 
-    spr_force = -1 * globals.agent_k * disp * normalized
-    """
-    print("-------------------------------------------------------")
-    print("This is force calculated directly: " + str(-1 * globals.agent_k * disp))
-    """
-    if (globals.potential_switch == 1):
-        spr_pot = ((globals.agent_k * (disp ** 2)) / 2)
-        """
-        h = 1
-        print("This is force from potential h =  " + str(h) + ": " + str(-(((globals.agent_k * ((disp + h) ** 2)) / 2) - ((globals.agent_k * ((disp - h) ** 2)) / 2)) / 2 * h))
-        """
-    else:
-        pass
-
+    globals.spr_force = -1 * globals.agent_k * disp * normalized
 
     if (dev_analyze == "y"):
         ## For debugging purposes.
         print("LJ:    " + str(globals.lj_force))
-        print("SPR:    " + str(spr_force))
-        print(np.shape(spr_force))
+        print("SPR:    " + str(globals.spr_force))
+        print(np.shape(globals.spr_force))
     elif (dev_analyze == "n"):
         pass
 
-
-    agent_force = globals.lj_force + spr_force
-    globals.agent_pot.append(lj_pot + spr_pot)
-
-
-    ff = spr_force[0][0] - globals.lj_force[0][0]
-    ##ff = spr_force[0][0]
-    globals.fric.append(ff)
-
-    ##print(globals.agent_pot)
-
+    agent_force = globals.lj_force + globals.spr_force
 
     return agent_force
 
@@ -102,7 +75,7 @@ def SubstrateForce():
 
     neighbor = Substrate.R[list(Substrate.N[Substrate.trap])]
     atom = Substrate.R[Substrate.trap].reshape((neighbor.shape[0], 1, 3))
-    norm = np.linalg.norm(neighbor - atom, axis=2)[:, np.newaxis]
+    norm = np.linalg.norm(neighbor - atom, axis = 2)[:, np.newaxis]
 
     subs_force[Substrate.trap] = np.squeeze(Substrate.k * (norm - Substrate.latt_const) / norm @ (neighbor - atom), axis=1)
 
