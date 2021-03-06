f06f5b5ca20c05d9de4e4a097b797d7a71d33d6c
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2021-03-07 01:12:06 +0300

    3D Neighbortable is fixed

--- classes/substrate.py
+++ classes/substrate.py
@@ -10,7 +10,6 @@ Call find_neighbor first, then init_disp.
 
 ---------------------------------
 """
-## it's to be modified for periodic conditions.
 
 _, _, _, subs_param, _, _ = parse('input.txt')
 class Substrate():
@@ -20,43 +19,45 @@ class Substrate():
         self.dim = int(subs_param['dim'])
         self.num = int(subs_param['num'])
         self.mass = float(subs_param['mass'])
-        self.layers = int(subs_param['layers'])
+        layers = int(subs_param['layers'])
         self.bound_cond = subs_param['bound_cond']
         self.displace_type = subs_param['displace_type']
         self.latt_const = float(subs_param['latt_const'])
         self.cuto_const = float(subs_param['cuto_const'])
+        fix_layers = int(subs_param['fix_layers'])
         self.L = self.num * self.latt_const
         
         # initialize position and trap
         if self.dim == 1:
-            zgrid, ygrid, xgrid = np.mgrid[0:self.layers, 0:1, 0:self.num] * self.latt_const
+            zgrid, ygrid, xgrid = np.mgrid[0:layers, 0:1, 0:self.num] * self.latt_const
             Rx, Ry, Rz = np.vstack(xgrid.ravel()), np.vstack(ygrid.ravel()), np.vstack(zgrid.ravel())
             self.R = np.hstack((Rx, Ry, Rz))
 
             if self.bound_cond == 'fixed':
                 self.trap = np.where(\
-                    (self.R[:, 0] != 0) & (self.R[:, 0] != (self.num-1)*self.latt_const))
+                    (self.R[:, 0] != 0) & (self.R[:, 0] != (self.num-1)*self.latt_const))[0]
+            elif self.bound_cond == 'periodic':
+                self.trap = np.arange(0, self.R.shape[0])
 
         elif self.dim == 2:
-            zgrid, ygrid, xgrid = np.mgrid[0:self.layers, 0:self.num, 0:self.num] * self.latt_const
+            zgrid, ygrid, xgrid = np.mgrid[0:layers, 0:self.num, 0:self.num] * self.latt_const
             Rx, Ry, Rz = np.vstack(xgrid.ravel()), np.vstack(ygrid.ravel()), np.vstack(zgrid.ravel())
             self.R = np.hstack((Rx, Ry, Rz))
 
             if self.bound_cond == 'fixed':
                 self.trap = np.where(\
                     (self.R[:, 0] != 0) & (self.R[:, 0] != (self.num-1) * self.latt_const) & \
-                    (self.R[:, 1] != 0) & (self.R[:, 1] != (self.num-1) * self.latt_const))
+                    (self.R[:, 1] != 0) & (self.R[:, 1] != (self.num-1) * self.latt_const))[0]
+            elif self.bound_cond == 'periodic':
+                self.trap = np.arange(0, self.R.shape[0])
 
         elif self.dim == 3:
-            zgrid, ygrid, xgrid = np.mgrid[0:self.layers, 0:self.num, 0:self.num] * self.latt_const
+            zgrid, ygrid, xgrid = np.mgrid[0:layers, 0:self.num, 0:self.num] * self.latt_const
             Rx, Ry, Rz = np.vstack(xgrid.ravel()), np.vstack(ygrid.ravel()), np.vstack(zgrid.ravel())
             self.R = np.hstack((Rx, Ry, Rz))
 
-            if self.bound_cond == 'fixed':
-                self.trap = np.where(\
-                    (self.R[:, 0] != 0) & (self.R[:, 0] != (self.num-1) * self.latt_const) & \
-                    (self.R[:, 1] != 0) & (self.R[:, 1] != (self.num-1) * self.latt_const) & \
-                    (self.R[:, 2] != 0) & (self.R[:, 2] != (self.layers-1) * self.latt_const))
+            self.trap = np.where(np.isin(self.R[:, 2], np.arange(fix_layers)*self.latt_const) == False)[0]
+            self.numlayer = int(self.R.shape[0] / layers)
 
         # initialize velocity and acceleration
         self.V = np.zeros(np.shape(self.R))
@@ -67,10 +68,9 @@ class Substrate():
             dR = distance.cdist(self.R, self.R, 'euclidean')
                 
         elif self.bound_cond == 'periodic':
-            self.trap = np.arange(0, self.R.shape[0])
-            X = self.R[:, 0].reshape(self.R[:, 0].size, 1)
-            Y = self.R[:, 1].reshape(self.R[:, 1].size, 1)
-            Z = self.R[:, 2].reshape(self.R[:, 2].size, 1)
+            X = self.R[:, 0][np.newaxis]
+            Y = self.R[:, 1][np.newaxis]
+            Z = self.R[:, 2][np.newaxis]
             dX, dY, dZ = X - X.T, Y - Y.T, Z - Z.T
             Xbound, Ybound, Zbound = (abs(dX) > self.L/2), (abs(dY) > self.L/2), (abs(dZ) > self.L/2)
             Xcopy, Ycopy, Zcopy = dX * Xbound, dY * Ybound, dZ * Zbound
@@ -79,40 +79,37 @@ class Substrate():
             dZ += (-np.sign(Zcopy)) * self.L
             dR = np.sqrt(dX**2 + dY**2 + dZ**2)
 
-        cutoff = (dR != 0) & (dR < self.cuto_const)
+        cutoff = (dR[self.trap] != 0) & (dR[self.trap] < self.cuto_const)
         extract = np.where(cutoff == True)
         idx = np.unique(extract[0], return_index=True)
         self.N = np.array(np.split(extract[1], idx[1])[1:])
-        self.dR = dR * cutoff
-
+        if self.dim == 3:
+            N = np.zeros((self.N.size, self.N[0].size))
+            N[:-self.numlayer] = np.array(list(self.N[:-self.numlayer]))
+            N[-self.numlayer:, :self.N[-1].size] = np.array(list(self.N[-self.numlayer:]))
+            N[-self.numlayer:, -1] = np.arange(self.R.shape[0]-self.numlayer, self.R.shape[0])
+            self.N = N.astype(np.int32)
+            
     def init_disp(self):
         if self.displace_type == 'random':
             if self.dim == 1:
-                if self.bound_cond == 'fixed':
-                    self.R[self.trap, 0:2] += (np.random.rand(*np.shape(self.R[self.trap, 0:2])) - 0.5) * 0.1
-
-                elif self.bound_cond == 'periodic':
-                    self.R[:, 0:2] += (np.random.rand(*np.shape(self.R[:, 0:2])) - 0.5) * 0.1
+                self.R[self.trap, 0:2] += (np.random.rand(*np.shape(self.R[self.trap, 0:2])) - 0.5) * 0.1
 
             elif self.dim != 1:
-                if self.bound_cond == 'fixed':
-                    self.R[self.trap] += (np.random.rand(*np.shape(self.R[self.trap])) - 0.5) * 0.1
-
-                elif self.bound_cond == 'periodic':
-                    self.R += (np.random.rand(*np.shape(self.R)) - 0.5) * 0.1
+                self.R[self.trap] += (np.random.rand(*np.shape(self.R[self.trap])) - 0.5) * 0.1
             
         elif self.displace_type == 'sinusoidal':
             print('Sinusoidal displacement is not implemented yet.\n')
             choice = input("If you want to continue with the default displacement 'random', press enter.\nIf not, write 'quit'.")
             if choice == "":
-                self.R = self.R + (np.random.rand(*np.shape(self.R)) - 0.5) * 0.1
+                self.R += (np.random.rand(*np.shape(self.R)) - 0.5) * 0.1
             elif choice == "quit":
                 return
             else:
                 print('You either made an invalid request or the property you requested must be written differently.\n')
                 choice = input("If you want to continue with the default displacement 'random', press enter.\nIf not, write 'quit'.")
                 if choice == "":
-                    self.R = self.R + (np.random.rand(*np.shape(self.R)) - 0.5) * 0.1
+                    self.R += (np.random.rand(*np.shape(self.R)) - 0.5) * 0.1
                 elif choice == "quit":
                     return
                 else:
@@ -122,7 +119,7 @@ class Substrate():
             print('You either made an invalid request or the property you requested must be written differently.\n')
             choice = input("If you want to continue with the default displacement 'random', press enter.\nIf not, write 'quit'.")
             if choice == "":
-                self.R = self.R + (np.random.rand(*np.shape(self.R)) - 0.5) * 0.1
+                self.R += (np.random.rand(*np.shape(self.R)) - 0.5) * 0.1
             elif choice == "quit":
                 return
             else:
