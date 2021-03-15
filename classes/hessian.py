90c9704120a81e63a2fd2f5e9315be4cdc5ef8dd
Bartu Yaman <e235581@metu.edu.tr>
2021-03-15 20:07:17 +0300

    Update hessian.py

--- classes/hessian.py
+++ classes/hessian.py
@@ -11,9 +11,27 @@ dim = 2
 """ lemme psuedocode here
 pos_plus = pos[j] + h
 pos_minus = pos[j] - h
-
 hessian[i][j] = (F(pos_plus)[i] - F(pos_minus)[i]) / (2 * h)
 """
+def SubstrateForceH(R):
+    subs_force = np.zeros(R.shape)
+
+    R_N = R[Subs.N]
+    R_A = R[Subs.trap].reshape((R_N.shape[0], 1, 3))
+    dist = R_N - R_A
+    dist[dist > Subs.L/2] -= Subs.L
+    dist[dist < -Subs.L/2] += Subs.L
+    norm = LA.norm(dist, axis=2)[:, np.newaxis]
+    norm[norm[:, :, -1] == 0, -1] = Subs.latt_const
+
+    dR = (norm - Subs.latt_const) / norm @ dist
+    subs_force[Subs.trap] = np.squeeze(Subs.k * dR, axis=1)
+
+    lj_force = globals.lj_force
+    subs_force_fin = subs_force - lj_force
+
+    return subs_force_fin
+
 
 def Hessian():
     # Flatten the position array for easier indexing.
@@ -29,15 +47,16 @@ def Hessian():
 
             # Calculate the "plus" force element for differentiation later on.
             subs_pos_flat[j] = pos_plus
-            plus_pos_mat = np.reshape(subs_pos_flat, (globals.num, 3))
-            plus_force_calc = SubstrateForce(plus_pos_mat) # The force function needs to take the altered position element as an input here.
+            plus_pos_mat = np.reshape(subs_pos_flat, (globals.num * globals.num, 3))
+            plus_force_calc = SubstrateForceH(plus_pos_mat) # The force function needs to take the altered position element as an input here.
             plus_force_flat = plus_force_calc.flatten()
             plus_force = plus_force_flat[i]
 
+
             # Calculate the "minus" force element for differentiation later on.
             subs_pos_flat[j] = pos_minus
-            min_pos_mat = np.reshape(subs_pos_flat, (globals.num, 3))
-            minus_force_calc = SubstrateForce(min_pos_mat) # The force function needs to take the altered position element as an input here.
+            min_pos_mat = np.reshape(subs_pos_flat, (globals.num * globals.num, 3))
+            minus_force_calc = SubstrateForceH(min_pos_mat) # The force function needs to take the altered position element as an input here.
             minus_force_flat = minus_force_calc.flatten()
             minus_force = minus_force_flat[i]
 
@@ -50,4 +69,6 @@ def Hessian():
     _, eigvec = LA.eig(hessian)
     vec_proj = np.dot(eigvec, Subs.R) / (LA.norm(eigvec) * LA.norm(Subs.R))
 
-    return hessian, vec_proj 
+    print(vec_proj)
+
+    return hessian, vec_proj
