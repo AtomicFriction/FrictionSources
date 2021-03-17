afc1f1115b9a555097327f8af834439bb69c0ced
Bartu Yaman <e235581@metu.edu.tr>
2021-03-17 21:42:06 +0300

    Update logger.py

--- classes/logger.py
+++ classes/logger.py
@@ -19,6 +19,7 @@ def InitializeLog():
 def LogProtocol(counter_i):
     with open("log.txt", "a") as file_1:
         file_1.write("Protocol Step " + str(globals.run[counter_i]) + "\n")
+        file_1.write("Step")
         if ("ff" in globals.data):
             globals.ff_switch = 1
             file_1.write("              " + "ff")
@@ -39,19 +40,19 @@ def LogProtocol(counter_i):
 """
 -> Logs the each step.
 """
-def WriteLog(counter_i, counter_j, ff, pe, ke, etot, temp):
+def WriteLog(counter_i, counter_j):
     log = open('log.txt', 'a')
     if (counter_j <= globals.run[counter_i][2]):
-        log.write("Step " + str(counter_j) + ":    ")
+        log.write(str(counter_j) + "    ")
         if (globals.ff_switch == 1):
-            log.write(str(ff) + "   ")
+            log.write(str(globals.ff) + "   ")
         if (globals.potential_switch == 1):
-            log.write(str(pe) + "   ")
+            log.write(str(globals.pe) + "   ")
         if (globals.kinetic_switch == 1):
-            log.write(str(ke) + "   ")
+            log.write(str(globals.ke) + "   ")
         if (globals.etot_switch == 1):
-            log.write(str(etot) + "   ")
+            log.write(str(globals.etot) + "   ")
         if (globals.temp_switch == 1):
-            log.write(str(temp) + "   ")
+            log.write(str(globals.temp) + "   ")
 
         log.write("\n")
