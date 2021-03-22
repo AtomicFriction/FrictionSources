2c406cedeaa1360e7bc35bf1c694be7caa23f0fa
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2021-03-22 11:28:31 +0300

    Thermostat and integrator names are now checked

--- classes/input_parser/input_parser.py
+++ classes/input_parser/input_parser.py
@@ -1,4 +1,4 @@
-from input_parser.dtypes import typeof
+from input_profile import *
 import numpy as np
 
 def parse(file):
@@ -16,11 +16,14 @@ def parse(file):
     slid = dict(params[slid_ind+1:params.index(['/'], slid_ind)])
     thermo = dict(params[thermo_ind+1:params.index(['/'], thermo_ind)])
 
+    ### VALUE ERROR MUST BE TYPE ERROR
+    
     for key, val in gen.items():
         try: gen[key] = typeof[key](val)
         except ValueError: print('The input {} must be of type {}'.format(key, typeof[key])); exit()
+
     for key, val in prot.items():
-        if key != 'run':
+        if key != 'run' and key != 'integ':
             try: prot[key] = typeof[key](val)
             except ValueError: print('The input {} must be of type {}'.format(key, typeof[key])); exit()
         elif key == 'run':
@@ -28,6 +31,12 @@ def parse(file):
                 prot[key] = list(map(float, prot[key].split())); prot[key][2::3] = map(int, prot[key][2::3])
                 prot[key] = np.array(prot[key]).reshape(int(len(prot[key])/3), 3)
             except ValueError: print('The input run must consist of types (float, float, int), respectively.'); exit()
+        elif key == 'integ':
+            for integrator in list(integtype.keys()):
+                if val in integrator:
+                    try: prot[key] = integtype.get(integrator)
+                    except: print('Undefined integrator') # error type is to be specified
+
     for key, val in anal.items():
         if key != 'data':
             try: anal[key] = typeof[key](val)
@@ -35,9 +44,11 @@ def parse(file):
         elif key == 'data':
             try: anal[key] = list(map(str, anal[key].split()))
             except ValueError: print('The input {} must consist of type {}'.format(key, typeof[key])); exit()
+
     for key, val in subs.items():
         try: subs[key] = typeof[key](val)
         except ValueError: print('The input {} must be of type {}'.format(key, typeof[key])); exit()
+
     for key, val in slid.items():
         if key != 'agent_pos' and key != 'slider_pos' and key != 'slider_vel':
             try: slid[key] = typeof[key](val)
@@ -45,16 +56,25 @@ def parse(file):
         elif key == 'agent_pos' or key == 'slider_pos' or key == 'slider_vel':
             try: slid[key] = np.array(list(map(float, slid[key].split())))
             except ValueError: print('The input {} must consist of type {}'.format(key, typeof[key])); exit()
+
     for key, val in thermo.items():
         try: thermo[key] = typeof[key](val)
         except ValueError: print('The input {} must be of type {}'.format(key, typeof[key])); exit()
-
+        else:
+            if key == 'thermo':
+                for thermostat in list(thermotype.keys()):
+                    if val in thermostat:
+                        try: thermo[key] = thermotype.get(thermostat)
+                        except: print('Undefined thermostat') # error type is to be specified
+                    
     dim = subs['dim']
     if not dim >= 1 and dim <= 3: exit('Unexpected dimension')
-    elif dim == 1 or dim == 2:
-        subs['layers'] = 1
-        subs['fix_layers'] = 0
-    elif dim == 3 and subs['bound_cond'] == 'fixed':
-        subs['bound_cond'] = 'periodic'
 
-    return gen, prot, anal, subs, slid, thermo
+    elif dim == 1 or dim == 2: subs['layers'] = 1; subs['fix_layers'] = 0
+
+    elif dim == 3 and subs['bound_cond'] == 'fixed': subs['bound_cond'] = 'periodic'
+
+    elif thermo['mode'] == 'trap' and not thermo['thickness'] > 0: 
+        raise ValueError('Thickness must be a positive integer')
+
+    return gen, prot, anal, subs, slid, thermo 
