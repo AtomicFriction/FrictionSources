8eb3bbf447a587b55837ea80ad622c3a92021d33
Bartu Yaman <e235581@metu.edu.tr>
2021-03-31 23:11:55 +0300

    Update eigtest.py

--- classes/eigtest.py
+++ classes/eigtest.py
@@ -1,32 +1,34 @@
 import numpy as np
 import globals
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from substrate import Subs
+from mpl_toolkits.mplot3d import Axes3D
 
-#plt.style.use('ggplot')
 
-eigvec_all = np.loadtxt('eigtest.txt')
-print(np.shape(eigvec_all))
+eigvec_all = np.load('eigtest.npy')
 
-eigvec = eigvec_all[:, 0]
+eigvec = eigvec_all[:, 100]
 
 reshaped_eigvec = np.reshape(eigvec, (globals.num * globals.num, 3))
-print(np.shape(reshaped_eigvec))
-
 
 R_disp = []
-t = []
 
 fig = plt.figure(figsize=(15,7))
-ax = fig.add_subplot(111)
-
+ax = fig.add_subplot(111, projection = "3d")
 for a in np.arange(-10, 10, 1):
-    plt.ion()
-    R_disp = (Subs.R + (reshaped_eigvec * a))
 
-    ax.plot(R_disp[:,0], color = "blue", label = "0")
-    ax.plot(R_disp[:,1], color = "red", label = "1")
-    ax.plot(R_disp[:,2], color = "cyan", label = "2")
+    ax.set(zlim = (-0.06, 0.06))
+    ax.set(xlim = (0, 20))
+    ax.set(ylim = (0, 20))
+
+    plt.ion()
+    R_disp = Subs.R + ((reshaped_eigvec * a))
+
+    #ax.plot_trisurf(R_disp[:,0].real, R_disp[:,1].real, R_disp[:,2].real, cmap=cm.inferno)
+
+    ax.scatter(R_disp[:,0], R_disp[:,1], R_disp[:,2], color = "red")
+
     plt.pause(0.5)
 
     plt.show()
