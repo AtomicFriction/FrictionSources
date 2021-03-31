cadb34903dcdcd3a2640e3741a9d8d11781794cb
Bartu Yaman <e235581@metu.edu.tr>
2021-03-31 23:12:24 +0300

    Update hessian.py

--- classes/hessian.py
+++ classes/hessian.py
@@ -2,9 +2,9 @@ import numpy as np
 import numpy.linalg as LA
 import globals
 from substrate import Subs
-from interactions import SubstrateForce
+#from interactions import SubstrateForce
 
-h = 1e-3
+h = 1e-8
 dim = 2
 
 
@@ -13,6 +13,8 @@ pos_plus = pos[j] + h
 pos_minus = pos[j] - h
 hessian[i][j] = (F(pos_plus)[i] - F(pos_minus)[i]) / (2 * h)
 """
+
+
 def SubstrateForceH(R):
     subs_force = np.zeros(R.shape)
 
@@ -27,35 +29,32 @@ def SubstrateForceH(R):
     dR = (norm - Subs.latt_const) / norm @ dist
     subs_force[Subs.trap] = np.squeeze(Subs.k * dR, axis=1)
 
-    lj_force = globals.lj_force
-    subs_force_fin = subs_force - lj_force
-
-    return subs_force_fin
+    return subs_force
 
 
 def GetEigen():
-    # Flatten the position array for easier indexing.
-    subs_pos_flat = Subs.R.flatten()
     # Create an empty array of proper size.
-    hessian = np.empty([3 * (globals.num ** (dim)), 3 * (globals.num ** (dim))])
+    hessian = np.empty([3 * (globals.num ** 2) * Subs.layers, 3 * (globals.num ** 2) * Subs.layers])
     # Nested loop used for indexing.
-    for i in range(3 * (globals.num ** (dim))):
-        for j in range(3 * (globals.num ** (dim))):
+    for i in range(3 * (globals.num ** 2) * Subs.layers):
+        for j in range(3 * (globals.num ** 2) * Subs.layers):
+            # Flatten the position array for easier indexing.
+            subs_pos_flat = Subs.R.flatten()
             # Define "plus" and "minus" position elements here.
             pos_plus = subs_pos_flat[j] + h
             pos_minus = subs_pos_flat[j] - h
 
             # Calculate the "plus" force element for differentiation later on.
             subs_pos_flat[j] = pos_plus
-            plus_pos_mat = np.reshape(subs_pos_flat, (globals.num, 3))
-            plus_force_calc = SubstrateForce(plus_pos_mat) # The force function needs to take the altered position element as an input here.
+            plus_pos_mat = np.reshape(subs_pos_flat, ((globals.num ** 2) * Subs.layers, 3))
+            plus_force_calc = SubstrateForceH(plus_pos_mat) # The force function needs to take the altered position element as an input here.
             plus_force_flat = plus_force_calc.flatten()
             plus_force = plus_force_flat[i]
 
             # Calculate the "minus" force element for differentiation later on.
             subs_pos_flat[j] = pos_minus
-            min_pos_mat = np.reshape(subs_pos_flat, (globals.num, 3))
-            minus_force_calc = SubstrateForce(min_pos_mat) # The force function needs to take the altered position element as an input here.
+            min_pos_mat = np.reshape(subs_pos_flat, ((globals.num ** 2) * Subs.layers, 3))
+            minus_force_calc = SubstrateForceH(min_pos_mat) # The force function needs to take the altered position element as an input here.
             minus_force_flat = minus_force_calc.flatten()
             minus_force = minus_force_flat[i]
 
@@ -64,11 +63,12 @@ def GetEigen():
 
             # Insert the calculated value to its place inside the Hessian matrix.
             hessian[i][j] = ij_val
+
     _, eigvec = LA.eig(hessian)
-    
+
     return eigvec
 
-def ProjectEigen(eigvec):
-    
-    vec_proj = np.dot(eigvec, Subs.R) / (LA.norm(eigvec) * LA.norm(Subs.R))
+def ProjectEigen(eigvec, subs_pos):
+    subs_pos_flat = subs_pos.flatten()
+    vec_proj = np.dot(eigvec, subs_pos_flat) / (LA.norm(eigvec) * LA.norm(subs_pos))
     return vec_proj
