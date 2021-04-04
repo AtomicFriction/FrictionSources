2321bfa5118a427088c30735828b98e730b1cc53
Bartu Yaman <e235581@metu.edu.tr>
2021-04-04 23:15:56 +0300

    Update hessian.py

--- classes/hessian.py
+++ classes/hessian.py
@@ -2,10 +2,7 @@ import numpy as np
 import numpy.linalg as LA
 import globals
 from substrate import Subs
-#from interactions import SubstrateForce
-
-h = 1e-8
-dim = 2
+from interactions import SubstrateForce
 
 
 """ lemme psuedocode here
@@ -15,57 +12,65 @@ hessian[i][j] = (F(pos_plus)[i] - F(pos_minus)[i]) / (2 * h)
 """
 
 
-def SubstrateForceH(R):
-    subs_force = np.zeros(R.shape)
-
-    R_N = R[Subs.N]
-    R_A = R[Subs.trap].reshape((R_N.shape[0], 1, 3))
-    dist = R_N - R_A
-    dist[dist > Subs.L/2] -= Subs.L
-    dist[dist < -Subs.L/2] += Subs.L
-    norm = LA.norm(dist, axis=2)[:, np.newaxis]
-    norm[norm[:, :, -1] == 0, -1] = Subs.latt_const
-
-    dR = (norm - Subs.latt_const) / norm @ dist
-    subs_force[Subs.trap] = np.squeeze(Subs.k * dR, axis=1)
-
-    return subs_force
-
-
 def GetEigen():
+    h = 1e-5
     # Create an empty array of proper size.
     hessian = np.empty([3 * (globals.num ** 2) * Subs.layers, 3 * (globals.num ** 2) * Subs.layers])
     # Nested loop used for indexing.
     for i in range(3 * (globals.num ** 2) * Subs.layers):
         for j in range(3 * (globals.num ** 2) * Subs.layers):
+            print("Execution is at: i = " + str(i) + ", j = " + str(j))
             # Flatten the position array for easier indexing.
             subs_pos_flat = Subs.R.flatten()
+            print("Without alteration: " + str(subs_pos_flat[j]))
             # Define "plus" and "minus" position elements here.
             pos_plus = subs_pos_flat[j] + h
             pos_minus = subs_pos_flat[j] - h
 
             # Calculate the "plus" force element for differentiation later on.
             subs_pos_flat[j] = pos_plus
+            print("pos_plus = " + str(pos_plus))
             plus_pos_mat = np.reshape(subs_pos_flat, ((globals.num ** 2) * Subs.layers, 3))
-            plus_force_calc = SubstrateForceH(plus_pos_mat) # The force function needs to take the altered position element as an input here.
-            plus_force_flat = plus_force_calc.flatten()
+            print("plus_pos_mat = " + str(plus_pos_mat))
+            plus_force_calc = SubstrateForce(plus_pos_mat, Subs.bound, Subs.N, Subs.latt_const, Subs.k, Subs.L) # The force function needs to take the altered position element as an input here.
+            print("plus_force_calc = " + str(plus_force_calc))
+            plus_force_flat = plus_force_calc.ravel()
             plus_force = plus_force_flat[i]
+            print("plus_force[i] = " + str(plus_force))
 
             # Calculate the "minus" force element for differentiation later on.
             subs_pos_flat[j] = pos_minus
+            print("pos_minus = " + str(pos_minus))
             min_pos_mat = np.reshape(subs_pos_flat, ((globals.num ** 2) * Subs.layers, 3))
-            minus_force_calc = SubstrateForceH(min_pos_mat) # The force function needs to take the altered position element as an input here.
-            minus_force_flat = minus_force_calc.flatten()
+            print("min_pos_mat = " + str(min_pos_mat))
+            minus_force_calc = SubstrateForce(min_pos_mat, Subs.bound, Subs.N, Subs.latt_const, Subs.k, Subs.L) # The force function needs to take the altered position element as an input here.
+            print("minus_force_calc = " + str(minus_force_calc))
+            minus_force_flat = minus_force_calc.ravel()
             minus_force = minus_force_flat[i]
+            print("minus_force[i] = " + str(minus_force))
 
             # Perform the actual differentiation here.
-            ij_val = (plus_force - minus_force) / (2 * h)
+            ij_val = (plus_force - minus_force) / (pos_plus - pos_minus)
+            print("This is the second derivative ij: " + str(ij_val))
 
             # Insert the calculated value to its place inside the Hessian matrix.
             hessian[i][j] = ij_val
 
-    _, eigvec = LA.eig(hessian)
+    #hessian = 0.5 * (hessian + np.transpose(hessian))
 
+    _, eigvec = LA.eig(hessian)
+    """
+    print("//////////////////////////////////////////////////////////////////////////////")
+    print("//////////////////////////////////////////////////////////////////////////////")
+    print("//////////////////////////////////////////////////////////////////////////////")
+
+    print("Hessian: " + str(hessian))
+    print("//////////////////////////////////////////////////////////////////////////////")
+    print("//////////////////////////////////////////////////////////////////////////////")
+    print("//////////////////////////////////////////////////////////////////////////////")
+
+    print("For h = " + str(h) + ":    " + str(eigvec))
+    """
     return eigvec
 
 def ProjectEigen(eigvec, subs_pos):
