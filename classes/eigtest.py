b3e6e6e5eebd35ed27623691b09735474b760f29
Bartu Yaman <e235581@metu.edu.tr>
2021-04-04 23:16:11 +0300

    Update eigtest.py

--- classes/eigtest.py
+++ classes/eigtest.py
@@ -8,7 +8,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 eigvec_all = np.load('eigtest.npy')
 
-eigvec = eigvec_all[:, 100]
+eigvec = eigvec_all[:, 20]
 
 reshaped_eigvec = np.reshape(eigvec, (globals.num * globals.num, 3))
 
@@ -16,16 +16,17 @@ R_disp = []
 
 fig = plt.figure(figsize=(15,7))
 ax = fig.add_subplot(111, projection = "3d")
-for a in np.arange(-10, 10, 1):
+for a in np.arange(-1, 1, 0.1):
 
-    ax.set(zlim = (-0.06, 0.06))
-    ax.set(xlim = (0, 20))
-    ax.set(ylim = (0, 20))
+    #ax.set(zlim = (-0.06, 0.06))
+    #ax.set(xlim = (0, 20))
+    #ax.set(ylim = (0, 20))
+
 
     plt.ion()
     R_disp = Subs.R + ((reshaped_eigvec * a))
 
-    #ax.plot_trisurf(R_disp[:,0].real, R_disp[:,1].real, R_disp[:,2].real, cmap=cm.inferno)
+    ax.plot_trisurf(R_disp[:,0].real, R_disp[:,1].real, R_disp[:,2].real, cmap=cm.inferno)
 
     ax.scatter(R_disp[:,0], R_disp[:,1], R_disp[:,2], color = "red")
 
