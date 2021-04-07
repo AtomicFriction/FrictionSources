1d7dc1f419f9ec8a1380f0269be51e9a1ed6a0e0
Bartu Yaman <e235581@metu.edu.tr>
2021-04-07 23:24:52 +0300

    Update eigtest.py

--- classes/eigtest.py
+++ classes/eigtest.py
@@ -8,7 +8,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 eigvec_all = np.load('eigtest.npy')
 
-eigvec = eigvec_all[:, 20]
+eigvec = eigvec_all[:, 240]
 
 reshaped_eigvec = np.reshape(eigvec, (globals.num * globals.num, 3))
 
@@ -16,11 +16,11 @@ R_disp = []
 
 fig = plt.figure(figsize=(15,7))
 ax = fig.add_subplot(111, projection = "3d")
-for a in np.arange(-1, 1, 0.1):
+for a in np.arange(-10, 10, 1):
 
-    #ax.set(zlim = (-0.06, 0.06))
-    #ax.set(xlim = (0, 20))
-    #ax.set(ylim = (0, 20))
+    ax.set(zlim = (-0.5e1, 0.5e1))
+    ax.set(xlim = (0, 20))
+    ax.set(ylim = (0, 20))
 
 
     plt.ion()
@@ -28,9 +28,9 @@ for a in np.arange(-1, 1, 0.1):
 
     ax.plot_trisurf(R_disp[:,0].real, R_disp[:,1].real, R_disp[:,2].real, cmap=cm.inferno)
 
-    ax.scatter(R_disp[:,0], R_disp[:,1], R_disp[:,2], color = "red")
+    #ax.scatter(R_disp[:,0], R_disp[:,1], R_disp[:,2], color = "red")
 
-    plt.pause(0.5)
+    plt.pause(0.2)
 
     plt.show()
 
