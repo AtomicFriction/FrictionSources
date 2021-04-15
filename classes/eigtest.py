51bd89f3df5e65b9c37727fbe2a32eda0c5a7c3e
Bartu Yaman <e235581@metu.edu.tr>
2021-04-16 01:05:10 +0300

    Update eigtest.py

--- classes/eigtest.py
+++ classes/eigtest.py
@@ -5,33 +5,43 @@ from matplotlib import cm
 from substrate import Subs
 from mpl_toolkits.mplot3d import Axes3D
 
+eigvec_all = np.load('eigtest_eigvec.npy')
 
-eigvec_all = np.load('eigtest.npy')
-
-eigvec = eigvec_all[:, 240]
-
-reshaped_eigvec = np.reshape(eigvec, (globals.num * globals.num, 3))
-
-R_disp = []
-
-fig = plt.figure(figsize=(15,7))
-ax = fig.add_subplot(111, projection = "3d")
-for a in np.arange(-10, 10, 1):
-
-    ax.set(zlim = (-0.5e1, 0.5e1))
-    ax.set(xlim = (0, 20))
-    ax.set(ylim = (0, 20))
+eigval_all = np.load('eigtest_eigval.npy')
+idx = eigval_all.argsort()[::-1]
+eigval_all = eigval_all[idx]
+eigvec_all = eigvec_all[:,idx]
 
 
-    plt.ion()
-    R_disp = Subs.R + ((reshaped_eigvec * a))
 
-    ax.plot_trisurf(R_disp[:,0].real, R_disp[:,1].real, R_disp[:,2].real, cmap=cm.inferno)
+def eigtest(eigvec):
 
-    #ax.scatter(R_disp[:,0], R_disp[:,1], R_disp[:,2], color = "red")
+    eigvec_pl = eigvec[:, 0] ## 223
 
-    plt.pause(0.2)
+    reshaped_eigvec = np.reshape(eigvec_pl, (globals.num * globals.num, 3))
 
-    plt.show()
+    R_disp = []
 
-    plt.cla()
+    fig = plt.figure(figsize=(15,7))
+    ax = fig.add_subplot(111, projection = "3d")
+    for a in np.arange(-10, 10, 1):
+
+        ax.set(zlim = (-0.3e1, 0.3e1))
+        ax.set(xlim = (0, 20))
+        ax.set(ylim = (0, 20))
+
+
+        plt.ion()
+        R_disp = Subs.R + ((reshaped_eigvec * a))
+
+        ax.plot_trisurf(R_disp[:,0], R_disp[:,1], R_disp[:,2], cmap=cm.inferno)
+
+        ax.scatter(R_disp[:,0], R_disp[:,1], R_disp[:,2], color = "red")
+
+        plt.pause(0.1)
+
+        plt.show()
+
+        plt.cla()
+
+eigtest(eigvec_all)
