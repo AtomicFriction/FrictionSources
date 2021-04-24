d8f26d19613f6e39397ea4cba54f975941e88e1f
Bartu Yaman <e235581@metu.edu.tr>
2021-04-24 23:00:34 +0300

    Update hessian.py

--- classes/hessian.py
+++ classes/hessian.py
@@ -8,58 +8,6 @@ from substrate import Subs
 from interactions import SubstrateForce
 
 
-""" lemme psuedocode here
-pos_plus = pos[j] + h
-pos_minus = pos[j] - h
-hessian[i][j] = (F(pos_plus)[i] - F(pos_minus)[i]) / (2 * h)
-"""
-
-# These functions are here to make the analysis process easier. They will be removed later on.
-
-# Creates an array of tuples that can be used to print forces and the atoms that those forces belong to.
-def IndexedArray(arr):
-    num_elements = sum(len(x) for x in arr)
-    flat_ind = np.arange(0, num_elements / 3)
-    ind = np.reshape(flat_ind, ((globals.num ** 2) * Subs.layers, 1))
-    indexed = []
-    for i in range(len(arr)):
-        indexed.append((list(arr[i]), list(ind[i])))
-    list_indexed = list(indexed)
-    print(list_indexed)
-
-
-# Simple divider to organize the prints.
-def Divider():
-    print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
-    print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-
-
-# Prints the properties of the effected atom.
-def FindEffectedComponent(ind):
-    remainder = ind % 3
-    atom_number = (ind - remainder) / 3
-    component = {"0":"x", "1":"y", "2":"z"}
-    print("Atom " + str(atom_number) + " " + component[str(remainder)] + " component.")
-    return atom_number
-
-
-# Shows the whereabouts of the atom in question.
-def ShowWhere(number_of_atoms, atom_number):
-    arr = np.zeros((number_of_atoms))
-    arr[int(atom_number)] = 1
-    shaped = np.reshape(arr, (int(sqrt(number_of_atoms)), int(sqrt(number_of_atoms))))
-    print(shaped)
-
-
-def check_symmetric(M, rtol=1e-05, atol=1e-08):
-    return np.allclose(M, M.T, rtol=rtol, atol=atol)
-
-
-def check_diagonal(M):
-    i, j = np.nonzero(M)
-    return np.all(i == j)
-
-
 def GetEigen():
     h = 1e-3
     # Create an empty array of proper size.
@@ -74,7 +22,7 @@ def GetEigen():
             ShowWhere((globals.num ** 2) * Subs.layers, atom_num)
             """
             # Flatten the position array for easier indexing.
-            subs_pos_flat = Subs.R[Subs.bound].flatten()
+            subs_pos_flat = Subs.R.flatten()
             #print("Without alteration: " + str(subs_pos_flat[j]))
             # Define "plus" and "minus" position elements here.
             pos_plus = subs_pos_flat[j] + h
@@ -82,16 +30,16 @@ def GetEigen():
 
             # Calculate the "plus" force element for differentiation later on.
             subs_pos_flat[j] = pos_plus
-            plus_pos_mat = (np.reshape(subs_pos_flat, (Subs.bound.shape[0], 3)))
-            plus_force_calc = SubstrateForce(plus_pos_mat, Subs.bound, Subs.N, Subs.latt_const, Subs.k, Subs.L) # The force function needs to take the altered position element as an input here.
+            plus_pos_mat = np.reshape(subs_pos_flat, (Subs.R.shape[0], 3))
+            plus_force_calc = SubstrateForce(plus_pos_mat, Subs.bound, Subs.N, Subs.latt_const, Subs.k, Subs.L)[Subs.bound] # The force function needs to take the altered position element as an input here.
             #IndexedArray(plus_force_calc)
             plus_force_flat = plus_force_calc.ravel()
             plus_force = plus_force_flat[i]
 
             # Calculate the "minus" force element for differentiation later on.
             subs_pos_flat[j] = pos_minus
-            min_pos_mat = np.reshape(subs_pos_flat, (Subs.bound.shape[0], 3))
-            minus_force_calc = SubstrateForce(min_pos_mat, Subs.bound, Subs.N, Subs.latt_const, Subs.k, Subs.L) # The force function needs to take the altered position element as an input here.
+            min_pos_mat = np.reshape(subs_pos_flat, (Subs.R.shape[0], 3))
+            minus_force_calc = SubstrateForce(min_pos_mat, Subs.bound, Subs.N, Subs.latt_const, Subs.k, Subs.L)[Subs.bound] # The force function needs to take the altered position element as an input here.
             #IndexedArray(minus_force_calc)
             minus_force_flat = minus_force_calc.ravel()
             minus_force = minus_force_flat[i]
@@ -107,11 +55,14 @@ def GetEigen():
 
     eigval, eigvec = LA.eig(hessian)
 
+    idx = eigval.argsort()[::1]
+    eigval = eigval[idx]
+    eigvec = eigvec[:,idx]
 
-    return eigval.real, eigvec.real
+    return eigval, eigvec
 
 """
-def ProjectEigen(eigvec, subs_pos, interval, eigvec_num, step_num):
+def ProjectEigen(eigvec, subs_pos, eigvec_num, interval, step_num):
     if (step_num % interval == 0):
         subs_pos_flat = subs_pos.flatten()
         eigvec_select = eigvec[:, 0:(eigvec_num + 1)]
