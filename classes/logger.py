9682c8d379d8fa345d6e39aa99aba3ccc637fa19
Bartu Yaman <e235581@metu.edu.tr>
2021-05-04 23:25:27 +0300

    Update logger.py

--- classes/logger.py
+++ classes/logger.py
@@ -1,12 +1,13 @@
 import globals
+import csv
 
 """
 -> Copies the contents of "input.txt" file to "log.txt".
 -> Needs to run only once, at the beginning of the code.
 """
-def InitializeLog():
+def InitLog():
     # Copy the contents of "input.txt" to "log.txt"
-    with open("log.txt", "w") as file_1, open('./input_parser/input.txt', 'r') as file_2:
+    with open("log.csv", "w") as file_1, open('./input_parser/input.txt', 'r') as file_2:
         for line in file_2:
             file_1.write(line)
         file_1.write("\n")
@@ -16,43 +17,57 @@ def InitializeLog():
 -> Writes the protocol step and the analyzed quantities.
 -> Needs to run "counter_i" times.
 """
-def LogProtocol(counter_i):
-    with open("log.txt", "a") as file_1:
+def ProtLog(counter_i):
+    with open("log.csv", "a") as file_1:
         file_1.write("Protocol Step " + str(globals.run[counter_i]) + "\n")
+        file_1.write("\n")
         file_1.write("Step")
         if ("ff" in globals.data):
             globals.ff_switch = 1
-            file_1.write("              " + "ff")
+            file_1.write("  " + "ff")
         if ("pe" in globals.data):
             globals.potential_switch = 1
-            file_1.write("              " + "pe")
+            file_1.write("      " + "pe")
         if ("ke" in globals.data):
             globals.kinetic_switch = 1
-            file_1.write("              " + "ke")
+            file_1.write("  " + "ke")
         if ("etot" in globals.data):
             globals.etot_switch = 1
-            file_1.write("              " + "etot")
+            file_1.write("  " + "etot")
         if ("temp" in globals.data):
             globals.temp_switch = 1
-            file_1.write("              " + "temp")
+            file_1.write("  " + "temp")
         file_1.write("\n")
 
 """
--> Logs the each step.
+-> Logs the desired analysis parameters each step.
 """
 def WriteLog(counter_i, counter_j):
-    log = open('log.txt', 'a')
+    log = open('log.csv', 'a')
     if (counter_j <= globals.run[counter_i][2]):
-        log.write(str(counter_j) + "    ")
+        log.write(str(counter_j) + "  ")
         if (globals.ff_switch == 1):
-            log.write(str(globals.ff) + "   ")
+            log.write(str(list(globals.ff)) + "  ")
         if (globals.potential_switch == 1):
-            log.write(str(globals.pe) + "   ")
+            log.write(str(globals.pe) + "  ")
         if (globals.kinetic_switch == 1):
-            log.write(str(globals.ke) + "   ")
+            log.write(str(globals.ke) + "  ")
         if (globals.etot_switch == 1):
-            log.write(str(globals.etot) + "   ")
+            log.write(str(globals.etot) + "  ")
         if (globals.temp_switch == 1):
-            log.write(str(globals.temp) + "   ")
+            log.write(str(globals.temp) + "  ")
 
         log.write("\n")
+
+
+def EigProjLogInit():
+    with open('eig_proj_log.csv', 'w') as proj_log:
+        proj_log.write("Eigenvector Projection Log")
+        proj_log.write("\n")
+
+
+def EigProjLog(counter_i, counter_j, proj):
+    with open('eig_proj_log.csv', 'a') as proj_log:
+        proj_log.write("Protocol: " + str(counter_i) + " step: " + str(counter_j) + "  ")
+        proj_log.write(str(list(proj)))
+        proj_log.write("\n")
