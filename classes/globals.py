fa798679ac490843820f5cf3b21db2a5030a2c31
Bartu Yaman <e235581@metu.edu.tr>
2021-05-06 12:03:56 +0300

    Update globals.py

--- classes/globals.py
+++ classes/globals.py
@@ -1,46 +1,48 @@
 from input_parser.input_parser import parse
 import numpy as np
-#from substrate import Subs
 
-##################################################
-##################################################
-gen_param, _, _, _, _, _ = parse('./input_parser/input.txt')
+
+# "general" parameters from the input file.
+gen_param, _, _, _, _, _ = parse('./input_parser/input.txt') # Parse the parameters.
 
 cutoff = gen_param['cutoff']
-##################################################
-##################################################
-_, prot_param, _, _, _, _ = parse('./input_parser/input.txt')
+
+
+# "protocol" parameters from the input file.
+_, prot_param, _, _, _, _ = parse('./input_parser/input.txt') # Parse the parameters.
 
 dt = prot_param['dt']
 integtype = prot_param['integ']
 run = prot_param['run']
 eig_proj = prot_param['eig_proj']
 apply_agent = prot_param['apply_agent']
-##################################################
-##################################################
-_, _, analysis_param, _, _, _ = parse('./input_parser/input.txt')
+
+
+# "analysis" parameters from the input file.
+_, _, analysis_param, _, _, _ = parse('./input_parser/input.txt') # Parse the parameters.
 data = analysis_param['data']
-##################################################
-##################################################
-_, _, _, subs_param, _, _ = parse('./input_parser/input.txt')
+
+
+# "substrate" parameters from the input file.
+_, _, _, subs_param, _, _ = parse('./input_parser/input.txt') # Parse the parameters.
 
 num = subs_param['num']
 subs_k = subs_param['k']
 latt_const = subs_param['latt_const']
-##################################################
-##################################################
-_, _, _, _, agent_param, _ = parse('./input_parser/input.txt')
+
+
+# "agent" parameters from the input file.
+_, _, _, _, agent_param, _ = parse('./input_parser/input.txt') # Parse the parameters.
 
 constrain = agent_param['constrain']
 eq_len = agent_param['eq_len']
-agent_k = agent_param['k']
-# The constant sigma for The Lennard Jones interaction.
-sigma = (agent_param['sigma'])
-# The constant epsilon for The Lennard Jones interaction.
-epsilon = (agent_param['epsilon'])
-##################################################
-##################################################
-_, _, _, _, _, thermo_param = parse('./input_parser/input.txt')
+agent_k = agent_param['k'] #
+sigma = (agent_param['sigma']) # The constant sigma for The Lennard Jones interaction.
+epsilon = (agent_param['epsilon']) # The constant epsilon for The Lennard Jones interaction.
+
+
+# "thermo" parameters from the input file.
+_, _, _, _, _, thermo_param = parse('./input_parser/input.txt') # Parse the parameters.
 
 thermotype = thermo_param['thermo']
 tau = thermo_param['tau']
@@ -49,8 +51,9 @@ gamma = thermo_param['gamma'],
 mode = thermo_param['mode']
 thickness = thermo_param['thickness']
 boltz = 8.617333262e-5
-##################################################
-##################################################
+
+
+# Switches for logging.
 pe_switch = 0
 pe = 0
 ke_switch = 0
@@ -64,17 +67,18 @@ etot = 0
 vec_proj = 0
 
 
-spr_force = np.zeros((1, 3))
-lj_force = np.zeros((num * num, 3))
-
+# Potential energy calculation variables.
 rr_12 = []
 rr_6 = []
 disp = 0
 subs_dR = 0
 
-# For Lennard-Jones calculations.
+
+# Agent force calculation variables.
 sig_12 = (sigma ** 12)
 sig_6 = (sigma ** 6)
+spr_force = np.zeros((1, 3))
+lj_force = np.zeros((num * num, 3))
 
 """
 agent_pot = subs_pot = fric = lj_force = np.zeros(np.sum(run[:, 2]))
@@ -83,6 +87,10 @@ agent_pot = subs_pot = fric = lj_force = np.zeros(np.sum(run[:, 2]))
 agent_pot = []
 subs_pot = []
 
+# Initial substrate position for evctor projection calculations.
+initial_Subs_R = 0
+
+# Command line arguement switches.
 save_progress = 0
 from_progress = False
 save_hess_eig = False
