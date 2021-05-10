9c379f3afe323488172a6ef22bd6f11fe86e0a9f
Bartu Yaman <e235581@metu.edu.tr>
2021-05-10 22:56:23 +0300

    Update main.py

--- classes/main.py
+++ classes/main.py
@@ -14,14 +14,12 @@ if __name__ == "__main__":
     # Definitions of different command line arguements.
     parser.add_argument("--save_progress", help = "Saves the state of the system every given step interval in case it gets interrupt somehow. For example, '-save_progress 10000' means that the state of the system will be saved every 10000 steps.")
     parser.add_argument("--from_progress", help = "Loads the state of the system from a saved file and continues to run the saved configuration. ", action="store_true")
-    parser.add_argument("--save_hess_eig", help = "Saves the eigenvalues and the eigenvectors of the Hessian matrix.", action="store_true")
     parser.add_argument("--animate", help = "Animates the system every given step interval. For example, '--animate 100' animates the system every 100 steps.")
     # Parse the arguements from the command line.
     args = parser.parse_args()
     # Save the parsed arguements into global variables.
     globals.save_progress = args.save_progress
     globals.from_progress = args.from_progress
-    globals.save_hess_eig = args.save_hess_eig
     globals.animate = args.animate
     print('Code execution started.')
     # Run the code.
