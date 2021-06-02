5c7d7033b986dce2a2517f0eb23c5075baee5ba3
Çağdaş Kılıç <61436359+pitorodoptusdaktus@users.noreply.github.com>
2021-06-02 22:16:50 +0300

    Traps are replaced with frames
    Trap: The atoms to whom thermostat is not applied.
    Frame: The atoms to whom thermostat is applied.

--- codebase/thermostats.py
+++ codebase/thermostats.py
@@ -11,20 +11,20 @@ is called, the constants must be also called.
 """
 
 
-def VelRescale(F, T_target, trap):
+def VelRescale(F, T_target, frame):
     """
     Updates only the velocity multiplying it by the factor 'L' calculated by using the ratio of target and instantaneous temperatures
-    Takes the parameters '(F, T_target, trap)'
-    Indexes the velocity array with the array 'trap', and updates only that part of the velocity array
+    Takes the parameters '(F, T_target, frame)'
+    Indexes the velocity array with the array 'frame', and updates only that part of the velocity array
     Returns both velocity and force arrays
     """
     L = (T_target / globals.temp) ** (1/2)
-    Subs.V[trap] *= L
+    Subs.V[frame] *= L
     F = F
     return Subs.V, F
 
 
-def Berendsen(F, T_target, trap):
+def Berendsen(F, T_target, frame):
     """
     -> Berendsen Thermostat.
     -> Calculates the instantaneous temperature of the system using the equipartition theorem, T_inst.
@@ -33,24 +33,24 @@ def Berendsen(F, T_target, trap):
     -> Returns a constant L to multiply with velocity of the particles in the system.
     """
     L = sqrt(1 + (globals.dt / globals.tau) * (T_target / globals.temp - 1))
-    Subs.V[trap] *= L
+    Subs.V[frame] *= L
     F = F
     return Subs.V, F
 
 
-def NoseHoover(F, T_target, trap):
-    N = F[trap].shape[0]
-    gamma_deriv = 1/globals.Q * (np.sum(Subs.mass * Subs.V[trap]**2) - 3*N * globals.boltz * T_target)
+def NoseHoover(F, T_target, frame):
+    N = F[frame].shape[0]
+    gamma_deriv = 1/globals.Q * (np.sum(Subs.mass * Subs.V[frame]**2) - 3*N * globals.boltz * T_target)
     gamma = integrate(gamma_deriv)
-    F[trap] -= gamma * Subs.V[trap]
+    F[frame] -= gamma * Subs.V[frame]
     Subs.V = Subs.V
     return Subs.V, F
 
 
 
-def Langevin(F, T_target, trap):
-    wiener = sqrt(globals.dt) * np.random.rand(*Subs.V[trap].shape)
-    F[trap] += (-1) * Subs.mass * globals.gamma * Subs.V[trap] + \
+def Langevin(F, T_target, frame):
+    wiener = sqrt(globals.dt) * np.random.rand(*Subs.V[frame].shape)
+    F[frame] += (-1) * Subs.mass * globals.gamma * Subs.V[frame] + \
         sqrt(2 * Subs.mass * globals.gamma * globals.boltz * T_target) * wiener
     Subs.V = Subs.V
     return Subs.V, F
