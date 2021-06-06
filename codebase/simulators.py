55c2ea3b5a49a6838fa83dc368868c2e24e8fda0
Bartu Yaman <e235581@metu.edu.tr>
2021-06-06 11:17:02 +0300

    Update simulators.py

--- codebase/simulators.py
+++ codebase/simulators.py
@@ -5,6 +5,9 @@ from substrate import Subs
 from interactions import AgentForce, SubstrateForce
 from integrators import Integrate
 from thermostats import ApplyThermo
+from hessian import GetEigen, ProjectEigen
+from logger import InitLog, ProtLog, WriteLog, EigProjLogInit, EigProjLog
+from analysis import Analysis, Temp
 
 
 """
@@ -12,12 +15,20 @@ Simulates the agent and slider atoms for every time step.
 Only takes the integrator as input.
 Performs a "status check" to see if the user wants to apply the agent on the substrate atoms.
 """
-def SimulateAgent(status, Integrate):
+def SimulateAgent(status, Integrate, i, j):
     # "on" choice simulates the agent normally.
     if (status == 1):
-        agent_force = AgentForce(Agent.R, Agent.slider_pos, Subs.R)
+        agent_force = AgentForce(Agent.R, Agent.slider_pos, Subs.R, None)
         Agent.AgentPeriodicity(Subs.L)
         Agent.slider_pos += Agent.slider_vel * globals.dt
+        # Calculate eigenvector projections.
+        if (j % globals.eig_proj[1] == 0):
+            proj = ProjectEigen(eigvec, Subs.R, Subs.bound, globals.initial_Subs_R, globals.eig_proj[0])
+            EigProjLog(i, j, proj)
+            # Run the necessary "analysis" functions.
+            Analysis()
+            # Write the wanted quatities to the log file.
+            WriteLog(i, j)
         return Integrate(agent_force, Agent.R, Agent.V, Agent.A, Agent.mass)
 
     # "off" choice virtually "lifts up" the agent from the substrate atoms, removing it from the system.
@@ -29,7 +40,6 @@ def SimulateAgent(status, Integrate):
 
 def SimulateSubs(T_target, ApplyThermo, Integrate):
     """Simulates substrate atoms for that time step using thermostat and integrator
-
     Takes the parameters '(T_target, ApplyThermo, Integrate)'
     Calls the function 'ApplyThermo' providing it with the function 'SubstrateForce',
     so that no requirement to define a variable for force
