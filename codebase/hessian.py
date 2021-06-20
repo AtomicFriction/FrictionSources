6e9612ae7c965c7e1d2962771957affbf5e0ee7e
Bartu Yaman <e235581@metu.edu.tr>
2021-06-21 00:31:39 +0300

    Update hessian.py

--- codebase/hessian.py
+++ codebase/hessian.py
@@ -7,7 +7,7 @@ import globals
 from substrate import Subs
 from interactions import SubstrateForce
 
-
+"""
 # These functions are here to make the analysis process easier. They will be removed later on.
 
 # Creates an array of tuples that can be used to print forces and the atoms that those forces belong to.
@@ -54,7 +54,7 @@ def check_symmetric(M, rtol=1e-05, atol=1e-08):
 def check_diagonal(M):
     i, j = np.nonzero(M)
     return np.all(i == j)
-
+"""
 
 def GetEigen():
     h = 1e-3
@@ -72,28 +72,22 @@ def GetEigen():
             full = np.copy(Subs.R)
             # Flatten the position array for easier indexing.
             subs_pos_flat = full[Subs.bound].flatten()
-            #print("Without alteration: " + str(subs_pos_flat[j]))
             pos_plus = subs_pos_flat[j] + h
             pos_minus = subs_pos_flat[j] - h
 
             # Calculate the "plus" force element for differentiation later on.
-            #print("initial " + str(subs_pos_flat[j]))
             subs_pos_flat[j] = pos_plus
-            #print("eklendikten sonra " + str(subs_pos_flat[j]))
             plus_pos_mat = np.reshape(subs_pos_flat, (Subs.bound.shape[0], 3))
             full[Subs.bound] = plus_pos_mat
             plus_force_calc = SubstrateForce(full, Subs.bound, Subs.N, Subs.latt_const, Subs.k, Subs.L)[Subs.bound]
-            #IndexedArray(plus_force_calc, Subs.bound.shape[0])
             plus_force_flat = plus_force_calc.flatten()
             plus_force = plus_force_flat[i]
 
             # Calculate the "minus" force element for differentiation later on.
             subs_pos_flat[j] = pos_minus
-            #print("çıkarmadan sonra " + str(subs_pos_flat[j]))
             min_pos_mat = np.reshape(subs_pos_flat, (Subs.bound.shape[0], 3))
             full[Subs.bound] = min_pos_mat
             minus_force_calc = SubstrateForce(full, Subs.bound, Subs.N, Subs.latt_const, Subs.k, Subs.L)[Subs.bound]
-            #IndexedArray(minus_force_calc, Subs.bound.shape[0])
             minus_force_flat = minus_force_calc.flatten()
             minus_force = minus_force_flat[i]
 
@@ -104,35 +98,9 @@ def GetEigen():
             # Insert the calculated value to its place inside the Hessian matrix.
             hessian[i][j] = -ij_val
 
-        #print(np.shape(hessian))
-
-
-
-
-    """
-    Fixed hessian is not symmetric, periodic hessian is.
-    """
-
-    #print(check_symmetric(hessian))
-    #print(check_diagonal(hessian))
-    """
-    for i in tqdm(range(3 * Subs.bound.shape[0])):
-        for j in range(3 * Subs.bound.shape[0]):
-            if (hessian[i][j] == hessian[j][i]):
-                print("EQUAL")
-            else:
-                print("NOT EQUAL AT i = " + str(i) + " and j = " + str(j))
-                print("ij val = " + str(hessian[i][j]))
-                print("ji val = " + str(hessian[j][i]))
-    """
-
-    """
-    There are non-equal ij-ji pairs in the hessian calculation bu this operation apparently fixes it.
-    This operation apparently solves the symmetricity problem as well.
-    """
+    # Symmetrize the matrix.
     hessian = 0.5 * (hessian + np.transpose(hessian))
-    #print(check_symmetric(hessian))
-
+    # Calculate the eigenvalues and eigenvectors.
     eigval, eigvec = LA.eig(hessian)
 
     # Ascending sort.
@@ -140,19 +108,4 @@ def GetEigen():
     eigvaln = eigval[idx]
     eigvecn = eigvec[:,idx]
 
-    """
-    for i in tqdm(range(3 * Subs.bound.shape[0])):
-        for j in range(3 * Subs.bound.shape[0]):
-            if (hessian[i][j] == hessian[j][i]):
-                print("EQUAL")
-            else:
-                print("NOT EQUAL AT i = " + str(i) + " and j = " + str(j))
-    """
-
     return eigvaln, eigvecn
-
-
-def ProjectEigen(eigvec, subs_pos, subs_bound, initial_pos, eigvec_num):
-    eigvec_select = eigvec[:, 0:(eigvec_num)]
-    vec_proj = abs(np.dot((subs_pos[subs_bound] - initial_pos[subs_bound]).ravel(), eigvec_select)) / (LA.norm(subs_pos[subs_bound]))
-    return vec_proj
