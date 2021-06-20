abe3a7fd01d3f252a953cd9147c66f8a3804798d
Bartu Yaman <e235581@metu.edu.tr>
2021-06-21 00:32:11 +0300

    Update logger.py

--- codebase/logger.py
+++ codebase/logger.py
@@ -22,17 +22,21 @@ def ProtLog(counter_i):
     with open("log.csv", "a") as file_1:
         file_1.write("Protocol Step " + str(globals.run[counter_i]) + "\n")
         file_1.write("\n")
+        file_1.write("\n")
         file_1.write("Step")
         if ("ff" in globals.data):
             globals.ff_switch = 1
             file_1.write("," + "ff_x")
             file_1.write("," + "ff_y")
             file_1.write("," + "ff_z")
+        if ("vf" in globals.data):
+            globals.vf_switch = 1
+            file_1.write("," + "vf")
         if ("pe" in globals.data):
-            globals.potential_switch = 1
+            globals.pe_switch = 1
             file_1.write("," + "pe")
         if ("ke" in globals.data):
-            globals.kinetic_switch = 1
+            globals.ke_switch = 1
             file_1.write("," + "ke")
         if ("etot" in globals.data):
             globals.etot_switch = 1
@@ -52,9 +56,11 @@ def WriteLog(counter_i, counter_j):
         log.write(str(counter_j) + ",")
         if (globals.ff_switch == 1):
             log.write(str(list(globals.ff)) + ",")
-        if (globals.potential_switch == 1):
+        if (globals.vf_switch == 1):
+            log.write(str((globals.vf)) + ",")
+        if (globals.pe_switch == 1):
             log.write(str(globals.pe) + ",")
-        if (globals.kinetic_switch == 1):
+        if (globals.ke_switch == 1):
             log.write(str(globals.ke) + ",")
         if (globals.etot_switch == 1):
             log.write(str(globals.etot) + ",")
