12c8bcc81c504dacefa033bd904095e6465a4667
Çağdaş Kılıç <61436359+KLCagdas@users.noreply.github.com>
2021-09-28 14:13:29 +0300

    Logger takes directories

--- codebase/logger.py
+++ codebase/logger.py
@@ -1,14 +1,12 @@
-import pandas as pd
 import numpy as np
 import globals
-import csv
 
 """
 -> Copies the contents of "input.txt" file to "log.txt".
 -> Needs to run only once, at the beginning of the code.
 """
-def InitLog():
-    with open('log.csv', 'w') as log, open('./input_parser/input.txt', 'r') as input:
+def InitLog(log_dir):
+    with open(log_dir, 'w') as log, open('./input_parser/input.txt', 'r') as input:
         # Copy the contents of "input.txt" to "log.txt"
         log.write(input.read() + '\n\n')
         # Write the header for the analysis part
@@ -17,8 +15,8 @@ def InitLog():
 """
 -> Logs the desired analysis parameters each step.
 """
-def WriteLog(prot, step):
-    with open('log.csv', 'a') as log:
+def WriteLog(log_dir, prot, step):
+    with open(log_dir, 'a') as log:
         np.savetxt(log, \
             np.array([prot, step, *globals.log_param.values()])[np.newaxis], delimiter=',')
 
@@ -27,24 +25,24 @@ def WriteLog(prot, step):
 """
 proj_pref = globals.eig_proj[0]
 # create a list of same headers
-proj_col = ['proj_'] * proj_pref
+proj_col = ['mod_'] * proj_pref
 # modify the headers to have sequential indices
 for idx, _ in enumerate(proj_col):
     proj_col[idx] = proj_col[idx].replace('_', str(idx))
 # unify the list of strings into one single string with commas as delimiters
 proj_col = ','.join(proj_col)
 
-def EigProjLogInit():
-    with open('eig_proj_log.csv', 'w') as proj_log:
+def EigProjLogInit(eig_dir):
+    with open(eig_dir, 'w') as proj_log:
         proj_log.write("Eigenvector Projection Log\n")
-        proj_log.write('protocol,step,{}\n'.format(proj_col))
+        proj_log.write('protocol,step,{}\n'.format(proj_col)) 
  
 
 """
 -> 
 """
-def EigProjLog(counter_i, step, proj):
-    with open('eig_proj_log.csv', 'a') as proj_log:
-        counter = np.array([counter_i, step])[np.newaxis]
+def EigProjLog(eig_dir, prot, step, proj):
+    with open(eig_dir, 'a') as proj_log:
+        counter = np.array([prot, step])[np.newaxis]
         np.savetxt(proj_log, \
             np.hstack((counter, proj[:proj_pref][np.newaxis])), delimiter=',')
