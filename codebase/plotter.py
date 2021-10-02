033f232901afce221ad14a22be82706b006c0753
Çağdaş Kılıç <61436359+KLCagdas@users.noreply.github.com>
2021-10-02 20:01:37 +0300

    Updated to be compatible with the results folder
    From now on, each plotter function must be called with the name of the related results folder, e.g. '10-02-2021-19-31-34'.

--- codebase/plotter.py
+++ codebase/plotter.py
@@ -2,26 +2,37 @@ import pandas as pd
 from input_parser.input_parser import parse
 import matplotlib.pyplot as plt
 
-_, prot_param, anal_param, _, _, _ = parse('./input_parser/input.txt')
+""" _, prot_param, anal_param, _, _, _ = parse('./input_parser/input.txt')
 t_steps = prot_param['run'][:, 2]
 apply_agent = prot_param['apply_agent']
-cols = ['prot', 'step', *anal_param['data']]
+cols = ['prot', 'step', *anal_param['data']]  """
 
-# this function is not ready
-def plot_anal_log(anal):
-    with open('./input_parser/input.txt', 'r') as file: 
-        len_input = len(file.readlines()) 
+def plot_anal_log(anal, result_folder):
+    # Define directories for the input file and the log file
+    inp_dir = './results/{}/input.txt'.format(result_folder)
+    log_dir = './results/{}/log.csv'.format(result_folder)
 
-    anal_df = pd.read_csv('log.csv', skiprows=len_input)
+    # Define a variable equal to the number of rows in the input file
+    with open(inp_dir, 'r') as file: len_input = len(file.readlines())
+
+    # Read the log file as a dataframe starting from where the input file ends
+    anal_df = pd.read_csv(log_dir, skiprows=len_input)
+
+    # Plot the dataframe
     anal_df.plot(x='step', y=anal, xlabel='Step', ylabel='Analyzed Quantities', title='Analyses of the Data')
     plt.show()
 
-plot_anal_log(['ff_x', 'ff_y', 'ff_z', 'vf', 'temp'])
+#plot_anal_log(['ff_x', 'ff_y', 'ff_z', 'vf', 'temp'], '10-02-2021-19-31-34')
 
-def plot_proj_log(proj):
-    proj_df = pd.read_csv('eig_proj_log.csv', skiprows=1) # read the file excluding the title
+def plot_proj_log(proj, result_folder):
+    # Define directories for the input file and the log file
+    eig_dir = './results/{}/eig_proj.csv'.format(result_folder)
+
+    # Read the eigen vector projection file as a dataframe excluding the title
+    proj_df = pd.read_csv(eig_dir, skiprows=1)
+
+    # Plot the dataframe
     proj_df.plot(x='step', y=proj, xlabel='Step', ylabel='Projection(s)', title='Eigenvector Projection')
     plt.show()
-    #print(df.get(df['protocol']==2))
 
-plot_proj_log(['mod5', 'mod6', 'mod97', 'mod89'])
+plot_proj_log(['mod5', 'mod6'], '10-02-2021-19-31-34')
