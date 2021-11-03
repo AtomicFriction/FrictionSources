6c4aaf08707bf741f7fafad9aa44d6a934048013
Bartu Yaman <e235581@metu.edu.tr>
2021-11-03 13:37:14 +0300

    Update globals.py

--- codebase/globals.py
+++ codebase/globals.py
@@ -1,22 +1,28 @@
-from input_parser.input_parser import parse
+# Library imports.
 import numpy as np
+
+
+# File imports.
+from input_parser.input_parser import parse
+
 
 
 # "general" parameters from the input file.
 gen_param, _, _, _, _, _ = parse('./input_parser/input.txt') # Parse the parameters.
 
 cutoff = gen_param['cutoff']
-eta = gen_param['eta']
 
 # "protocol" parameters from the input file.
 _, prot_param, _, _, _, _ = parse('./input_parser/input.txt') # Parse the parameters.
 
 dt = prot_param['dt']
-integtype = prot_param['integ']
+integtype_agent = prot_param['integ_agent']
+integtype_subs = prot_param['integ_subs']
 run = prot_param['run']
 eig_proj = prot_param['eig_proj']
 apply_agent = prot_param['apply_agent']
 apply_thermo = prot_param['apply_thermo']
+apply_damping = prot_param['apply_damping']
 
 
 # "analysis" parameters from the input file.
@@ -30,6 +36,7 @@ _, _, _, subs_param, _, _ = parse('./input_parser/input.txt') # Parse the parame
 num = subs_param['num']
 subs_k = np.array([subs_param['k1'], subs_param['k2'], subs_param['k3']])
 latt_const = subs_param['latt_const']
+subs_eta = subs_param['eta']
 
 
 # "agent" parameters from the input file.
@@ -40,6 +47,7 @@ eq_len = agent_param['eq_len']
 agent_k = agent_param['k'] #
 sigma = (agent_param['sigma']) # The constant sigma for The Lennard Jones interaction.
 epsilon = (agent_param['epsilon']) # The constant epsilon for The Lennard Jones interaction.
+agent_eta = agent_param['eta']
 
 
 # "thermo" parameters from the input file.
@@ -53,7 +61,8 @@ mode = thermo_param['mode']
 thickness = thermo_param['thickness']
 boltz = 8.617333262e-5
 
-
+for prot, step in enumerate(run):
+    print(prot, step)
 # Values for logging.
 log_param = {key: 0 for key in data}
 
@@ -95,3 +104,4 @@ load_eigs = False
 animate = False # This arguement needs an integer input form the user.
 save_progress_step = 0
 animate_step = 0
+
